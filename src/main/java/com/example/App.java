495cdd97e5feefd30c2d0ce1e8522161b6b7f376
Vignesh Bharathi <vigneshbharathi68@hotmail.com>
2025-11-21 08:43:21 +0530

    crud operation

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -21,13 +21,18 @@ public class App {
         String url = "jdbc:postgresql://localhost:5432/demo";
         String uname = "postgres";
         String pass = "1234";
-        String query = "select * from student";
+        String getAllRecordsQuery = "select * from student";
+        String insertQuery = "insert into student values (5, 'John', 48)";
+        String updateQuery = "update student set sname = 'Max' where sid = 1";
+        String deleteQuery = "delete student where sid = 1";
+        
         Class.forName("org.postgresql.Driver");
         Connection con = DriverManager.getConnection(url, uname, pass);
         Statement st = con.createStatement();
         System.out.println("Connection Established ");
 
-        ResultSet rs = st.executeQuery(query);
+        // Fetching all records
+        ResultSet rs = st.executeQuery(getAllRecordsQuery);
         while (rs.next()) {
             System.out.print(rs.getInt(1) + " - ");
             System.out.print(rs.getString(2) + " - ");
@@ -35,6 +40,19 @@ public class App {
             System.out.println();
 
         }
+
+        // Inserting data into a table
+        boolean insertStatus = st.execute(insertQuery);
+        System.out.println("Inserted document successfully" + insertStatus);
+        
+        // Update data into a table
+        boolean updateStatus = st.execute(updateQuery);
+        System.out.println("Updated document successfully" + updateStatus);
+        
+        // Delete a data in a table
+        boolean deleteStatus = st.execute(deleteQuery);
+        System.out.println("Updated document successfully" + deleteStatus);
+
         con.close();
         System.out.println("Connection closed");
 
